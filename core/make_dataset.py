00ac0155915c65c56648bbe3a481734824fce597
Dizitai <30825405+liuao520@users.noreply.github.com>
2020-12-03 10:26:25 +0800

    Update make_dataset.py

--- core/make_dataset.py
+++ core/make_dataset.py
@@ -19,7 +19,28 @@ class CocoDataset(object):
         return count
 
     def generate_dataset(self):
-        dataset = tf.data.TextLineDataset(filenames=self.data_dir)
-        dataset_length = self.__get_length_of_dataset(dataset)
-        dataset = dataset.batch(batch_size=self.config_params.BATCH_SIZE)
+        dataset = []
+        batch_data = []
+        with open(self.data_dir) as txtData:
+            lines = txtData.readlines()
+            for line in lines:
+              if len(batch_data) == self.config_params.BATCH_SIZE:
+                dataset.append(batch_data)
+                batch_data = []
+                batch_data.append(line)
+              else:
+                batch_data.append(line)
+                # for _ in range(self.config_params.BATCH_SIZE): #batch_size
+                #     batch_data.append(line)
+                # dataset.append(batch_data)
+                # batch_data.clear()
+
+        dataset_length = len(dataset)#self.__get_length_of_dataset(dataset)
+
         return dataset, dataset_length
+
+    # def generate_dataset(self):
+    #     dataset = tf.data.TextLineDataset(filenames=self.data_dir)
+    #     dataset_length = self.__get_length_of_dataset(dataset)
+    #     dataset = dataset.batch(batch_size=self.config_params.BATCH_SIZE)
+    #     return dataset, dataset_length
