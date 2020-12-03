42dc3232235ff98e65d7c9ba696b406173666ce2
Dizitai <30825405+liuao520@users.noreply.github.com>
2020-12-03 15:16:13 +0800

    Update loss_test.py

--- loss_test.py
+++ loss_test.py
@@ -15,54 +15,8 @@ from utils.tools import get_config_params
 
 from utils.work_flow import get_max_preds
 
-def __calculate_distance(pred, target, normalize):
-        pred = pred.astype(np.float32)
-        target = target.astype(np.float32)
-        distance = np.zeros((pred.shape[-1], pred.shape[0]))
-        for n in range(pred.shape[0]):
-            for c in range(pred.shape[-1]):
-                if target[n, 0, c] > 1 and target[n, 1, c] > 1:
-                    normed_preds = pred[n, :, c] / normalize[n]
-                    normed_targets = target[n, :, c] / normalize[n]
-                    distance[c, n] = np.linalg.norm(normed_preds - normed_targets)
-                else:
-                    distance[c, n] = -1
-        return distance
 
-def __distance_accuracy(self, distance):
-    distance_calculated = np.not_equal(distance, -1)
-    num_dist_cal = distance_calculated.sum()
-    if num_dist_cal > 0:
-        return np.less(distance[distance_calculated], 0.5).sum() * 1.0 / num_dist_cal
-    else:
-        return -1
-
-# @flow.global_function()
-def pck(network_output,target):
-    _, h, w, c = network_output.shape
-    index = list(range(c))
-    pred, _ = get_max_preds(heatmap_tensor=network_output)
-    target, _ = get_max_preds(heatmap_tensor=target)
-    normalize = np.ones((pred.shape[0], 2)) * np.array([h, w]) / 10
-    distance = self.__calculate_distance(pred, target, normalize)
-
-    accuracy = np.zeros((len(index) + 1))
-    average_accuracy = 0
-    count = 0
-
-    for i in range(c):
-        accuracy[i + 1] = self.__distance_accuracy(distance[index[i]])
-        if accuracy[i + 1] > 0:
-            average_accuracy += accuracy[i + 1]
-            count += 1
-    average_accuracy = average_accuracy / count if count != 0 else 0
-    if count != 0:
-        accuracy[0] = average_accuracy
-    return accuracy, average_accuracy, count, pred
-
-
-
-@flow.global_function()
+@flow.global_function(type="train")
 def test_job(
     images: tp.Numpy.Placeholder((10, 256, 256, 3), dtype=flow.float),
     target: tp.Numpy.Placeholder((10, 64, 64, 17), dtype=flow.float),
@@ -70,11 +24,7 @@ def test_job(
     # y_pred: tp.Numpy.Placeholder((10,64,64,17), dtype=flow.float),
     ) -> Tuple[tp.Numpy, tp.Numpy, tp.Numpy]:
     loss = JointsMSELoss()
-    pck = PCK()
-    # gt = GroundTruth(cfg, batch_data)
-    # images, target, target_weight = gt.get_ground_truth()
-    # with tf.GradientTape() as tape:
-    y_pred = HRNet(images, training=True)
+
     print("!!!!!!")
     print(y_pred.shape)
     loss_metric = loss.call(y_pred, target, target_weight)
@@ -86,26 +36,10 @@ def test_job(
     # print(loss_metric.shape)
     flow.optimizer.Adam(lr_scheduler, do_bias_correction=False).minimize(loss_metric)
 
-    # flow.optimizer.Adam(lr_scheduler=0.001).minimize(loss_metric)
-    # flow.losses.add_loss(loss_metric)
-    # flow.optimizer.Adam(lr_scheduler=1e-3).minimize(loss_metric)
     print("-----------------")
     print(loss_metric, y_pred, target)
     return loss_metric, y_pred, target
-    # _, accuracy_metric, _, _ = pck.call(network_output=y_pred, target=target)
-    # accuracy_metric = flow.math.reduce_mean(accuracy_metric)
-    # print(loss_metric,accuracy_metric)
-    # # flow.losses.add_loss(accuracy_metric)
-    # # print("Epoch: {}/{}, step: {}/{}, loss: {:.10f}, accuracy: {:.5f}".format(epoch,
-    # #                       cfg.EPOCHS,
-    # #                       step,
-    # #                       flow.math.ceil(dataset_length / cfg.BATCH_SIZE),
-    # #                       loss_metric,
-    # #                       accuracy_metric))
-    # flow.optimizer.Adam(lr_scheduler=1e-3).minimize(loss_metric)
 
-
-    # return outputs
 
 def metric(images,target,target_weight,y_pred):
   loss_metric, y_pred, target = test_job(images,target,target_weight)
@@ -129,5 +63,5 @@ if __name__ == "__main__":
     target = np.random.uniform(-10, 10, (10, 64, 64, 17)).astype(np.float32)
     target_weight = np.random.uniform(-10, 10, (10, 17, 1)).astype(np.float32)
     y_pred = np.random.uniform(-10, 10, (10,64,64,17)).astype(np.float32)
-    outputs = metric(images,target,target_weight,y_pred)
-    print(outputs)
+    metric(images,target,target_weight,y_pred)
+    
