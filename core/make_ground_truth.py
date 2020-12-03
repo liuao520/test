c5c38b2e73cae8f6e8be9041299c65253ead592f
Dizitai <30825405+liuao520@users.noreply.github.com>
2020-12-03 10:24:09 +0800

    Update make_ground_truth.py

--- core/make_ground_truth.py
+++ core/make_ground_truth.py
@@ -7,7 +7,7 @@ from utils.transforms import read_image, RandomCropTransform, ResizeTransform
 class GroundTruth(object):
     def __init__(self, config_params, batch_keypoints_info):
         self.config_params = config_params
-        self.batch_keypoints_list = self.__tensor2list(batch_keypoints_info)
+        self.batch_keypoints_list = batch_keypoints_info#self.__tensor2list(batch_keypoints_info)
         self.num_of_joints = config_params.num_of_joints
         self.image_size = np.array([config_params.IMAGE_HEIGHT, config_params.IMAGE_WIDTH])
         self.heatmap_size = np.array([config_params.HEATMAP_HEIGHT, config_params.HEATMAP_WIDTH])
@@ -39,13 +39,18 @@ class GroundTruth(object):
         batch_images = []
         for item in self.batch_keypoints_list:
             image, keypoints_3d, keypoints_3d_exist = self.__get_one_human_instance_keypoints(line_keypoints=item)
-            target, target_weight = self.__generate_target(keypoints_3d.numpy(), keypoints_3d_exist.numpy())
+            target, target_weight = self.__generate_target(np.array(keypoints_3d), np.array(keypoints_3d_exist))
             batch_images.append(image)
             batch_target.append(target)
+            # print(target.shape)
             batch_target_weight.append(target_weight)
-        batch_images_tensor = flow.stack(values=batch_images, axis=0)    # (batch_size, image_height, image_width, channels)
-        batch_target_tensor = flow.stack(values=batch_target, axis=0)    # (batch_size, heatmap_height, heatmap_width, num_of_joints)
-        batch_target_weight_tensor = flow.stack(values=batch_target_weight, axis=0)  # (batch_size, num_of_joints, 1)
+
+        batch_images_tensor = np.stack(batch_images, axis=0)
+        # print("batch_images_tensor形状:",batch_images_tensor.shape)    # (batch_size, image_height, image_width, channels)
+        batch_target_tensor = np.stack(batch_target, axis=0)    # (batch_size, heatmap_height, heatmap_width, num_of_joints)
+        
+        batch_target_weight_tensor = np.stack(batch_target_weight, axis=0)  # (batch_size, num_of_joints, 1)
+        # print(batch_images_tensor.shape, batch_target_tensor.shape, batch_target_weight_tensor.shape)
         return batch_images_tensor, batch_target_tensor, batch_target_weight_tensor
 
     def __get_one_human_instance_keypoints(self, line_keypoints):
@@ -55,17 +60,21 @@ class GroundTruth(object):
         _, bbox = self.__convert_string_to_float_and_int(split_line[3:7])
         keypoints, _ = self.__convert_string_to_float_and_int(split_line[7:])
         # keypoints_tensor = tf.convert_to_tensor(value=keypoints, dtype=tf.dtypes.float32)
-        keypoints_tensor = flow.tensor_buffer_to_tensor(keypoints, flow.float32)
-        keypoints_tensor = flow.reshape(keypoints_tensor, shape=(-1, 3))
+        keypoints_tensor = np.array(keypoints).astype(np.float32).reshape((-1, 3))
+        bbox = np.array(bbox)
+        # keypoints_tensor = flow.tensor_buffer_to_tensor(keypoints, flow.float32)
+        # keypoints_tensor = flow.reshape(keypoints_tensor, shape=(-1, 3))
 
         # Resize the image, and change the coordinates of the keypoints accordingly.
         image_tensor, keypoints = self.__image_and_keypoints_process(image_file, keypoints_tensor, bbox)
 
         keypoints_3d, keypoints_3d_exist = self.__get_keypoints_3d(keypoints)
+        # print(keypoints_3d.shape)
         return image_tensor, keypoints_3d, keypoints_3d_exist
 
     def __image_and_keypoints_process(self, image_dir, keypoints, bbox):
         image_tensor = read_image(image_dir, self.config_params)
+        # print(image_shape)
         if self.transform_method == "random crop":
             raise NotImplementedError("Not available temporarily.")
             # transform = RandomCropTransform(image=image_tensor, keypoints=keypoints, bbox=bbox, resize_h=self.image_size[0], resize_w=self.image_size[1], num_of_joints=self.num_of_joints)
@@ -85,16 +94,22 @@ class GroundTruth(object):
         keypoints_3d_exist_list = []
         for i in range(self.num_of_joints):
             # keypoints_3d_list.append(tf.convert_to_tensor([keypoints[i, 0], keypoints[i, 1], 0], dtype=tf.dtypes.float32))
-            keypoints_3d_list.append(flow.tensor_buffer_to_tensor([keypoints[i, 0], keypoints[i, 1], 0], flow.float32))
+            keypoints_3d_list.append(np.array([keypoints[i, 0], keypoints[i, 1], 0]).astype(np.float32))
             exist_value = keypoints[i, 2]
             if exist_value > 1:
                 exist_value = 1
             # exist_value: (1: exist , 0: not exist)
             # keypoints_3d_exist_list.append(tf.convert_to_tensor([exist_value, exist_value, 0], dtype=tf.dtypes.float32))
-            keypoints_3d_list.append(flow.tensor_buffer_to_tensor([exist_value, exist_value, 0], flow.float32))
+            keypoints_3d_exist_list.append(np.array([exist_value, exist_value, 0]).astype(np.float32))
 
-        keypoints_3d = flow.stack(values=keypoints_3d_list, axis=0)  # shape: (self.num_of_joints, 3)
-        keypoints_3d_exist = flow.stack(values=keypoints_3d_exist_list, axis=0)   # shape: (self.num_of_joints, 3)
+        # keypoints_3d = flow.stack(values=keypoints_3d_list, axis=0)  # shape: (self.num_of_joints, 3)
+        # keypoints_3d = keypoints_3d.reshape((self.num_of_joints, 3))
+        # print(keypoints_3d_list)
+        keypoints_3d = np.stack(keypoints_3d_list, axis=0)
+        # print("key:",keypoints_3d.shape)
+        # keypoints_3d_exist = flow.stack(values=keypoints_3d_exist_list, axis=0)   # shape: (self.num_of_joints, 3)
+        # print(keypoints_3d_exist_list)
+        keypoints_3d_exist = np.stack(keypoints_3d_exist_list, axis=0)
         return keypoints_3d, keypoints_3d_exist
 
     def __generate_target(self, keypoints_3d, keypoints_3d_exist):
@@ -128,10 +143,12 @@ class GroundTruth(object):
                 target[joint_id][img_y[0]:img_y[1], img_x[0]:img_x[1]] = g[g_y[0]:g_y[1], g_x[0]:g_x[1]]
 
         # target = tf.convert_to_tensor(target, tf.dtypes.float32)
-        target = flow.tensor_buffer_to_tensor(target, flow.float32)
-        target = flow.transpose(a=target, perm=[1, 2, 0])    # shape : (self.heatmap_size[0], self.heatmap_size[1], self.num_of_joints)
-        # target_weight = tf.convert_to_t?ensor(target_weight, tf.dtypes.float32)    # shape : (self.num_of_joints, 1)
-        target_weight = flow.tensor_buffer_to_tensor(target_weight, flow.float32)
+        target = np.array(target).astype(np.float32)
+        # target = flow.tensor_buffer_to_tensor(target, flow.float32)
+        target = np.transpose(a=target, axes=[1, 2, 0])    # s hape : (self.heatmap_size[0], self.heatmap_size[1], self.num_of_joints)
+        # target_weight = tf.convert_to_t?ensor(target_weight, tf.dtypes.float32)
+        target_weight  = np.array(target_weight).astype(np.float32)
+        # target_weight = flow.tensor_buffer_to_tensor(target_weight, flow.float32)
         return target, target_weight
 
 
